83fab67ce2601c5268ce6ec6fbc24ba20380b21c
Joaleramirez <jramirezfausto@csumb.edu>
2024-04-15 11:28:19 -0700

    Made the CreateAccount page

---
+++ app/src/main/java/I_choose_gachamon/CreateAccount.java
@@ -0,0 +1,16 @@
+package I_choose_gachamon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.i_choose_gacha_mon.R;
+
+public class CreateAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
