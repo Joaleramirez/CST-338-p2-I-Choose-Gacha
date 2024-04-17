a832139c810ba68dbe46078949577ab22ffc5ec1
ericr <ericrios1805@gmail.com>
2024-04-17 01:05:29 -0700

    Set Login button in MainActivity.java to lead to LandingPage.java

--- app/src/main/java/I_choose_gachamon/MainActivity.java
+++ app/src/main/java/I_choose_gachamon/MainActivity.java
@@ -2,15 +2,30 @@ package I_choose_gachamon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.i_choose_gacha_mon.R;
+import com.example.i_choose_gacha_mon.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.LoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, LandingPage.class));
+            }
+        });
     }
 }
