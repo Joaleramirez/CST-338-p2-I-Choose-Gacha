8c5177ffe316b0eaf90d84f7136522fdee8b3dad
ericr <ericrios1805@gmail.com>
2024-04-17 00:34:00 -0700

    Created initial LandingPage.java xml layout

---
+++ app/src/main/java/I_choose_gachamon/LandingPage.java
@@ -0,0 +1,21 @@
+package I_choose_gachamon;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.i_choose_gacha_mon.R;
+
+public class LandingPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing_page);
+
+    }
+}
