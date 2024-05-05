1f300203c13ff0b530ebc5b51aae0d77907a430c
Joaleramirez <jramirezfausto@csumb.edu>
2024-05-05 13:51:01 -0700

    made some gameplay mechanics

--- app/src/main/java/I_choose_gachamon/Gameplay.java
+++ app/src/main/java/I_choose_gachamon/Gameplay.java
@@ -3,6 +3,7 @@ package I_choose_gachamon;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,14 +15,21 @@ import com.example.i_choose_gacha_mon.R;
 import com.example.i_choose_gacha_mon.databinding.ActivityGameplayBinding;
 import com.example.i_choose_gacha_mon.databinding.ActivityGymBinding;
 
+import I_choose_gachamon.database.entities.Monster;
+
 public class Gameplay extends AppCompatActivity {
     ActivityGameplayBinding binding;
+    Monster monster;
+    TextView tvMonsterHp, tvMonsterEnergy;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityGameplayBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
+        monster = new Monster(1,"badguy", 100,10,1);
+        tvMonsterHp = findViewById(R.id.tvMonsterHp);
+        tvMonsterEnergy = findViewById(R.id.tvMonsterEnergy);
 
         binding.BackToMenuButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,5 +37,27 @@ public class Gameplay extends AppCompatActivity {
                 startActivity(new Intent(Gameplay.this, LandingPage.class));
             }
         });
+        binding.skill1ImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                monster.basicAttack(monster);// Adjust the target as needed
+                monster.chargeEnergy();
+                updateUI();
+            }
+        });
+        binding.skill2ImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (monster.getEnergy() >= 100) {
+                    monster.specialAttack(monster);
+                    updateUI();
+                }
+            }
+        });
+
     };
+    private void updateUI() {
+        tvMonsterHp.setText("HP: " + monster.getHp());
+        tvMonsterEnergy.setText("Energy: " + monster.getEnergy());
+    }
 }
